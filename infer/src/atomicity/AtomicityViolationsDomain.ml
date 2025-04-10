c02f1615a201bdeb574f83a1e1001a0c9585d8f8
Daniel Marek <danomarek8@gmail.com>
2025-04-11 00:45:06 +0200

    fix two tests, extend abstract state and summary with correct count set

--- infer/src/atomicity/AtomicityViolationsDomain.ml
+++ infer/src/atomicity/AtomicityViolationsDomain.ml
@@ -158,29 +158,67 @@ class atomic_pairs =
         In_channel.iter_lines ~fix_win_eol:true ic ~f:read_line ;
         In_channel.close ic )
 
-    (** Checks whether a calls pair is violating atomicity. *)
-    method check_violating_atomicity ?(check_first_empty : bool = false)
-        ((_, psnd) as p : calls_pair) ~(violations : Violations.t ref)
-        ~(atomic_last_pairs : AtomicPairSet.t) (loc : Location.t) : unit =
-      self#init ;
-      let check (p : calls_pair) (check_first_empty : bool) : unit =
-        let is_pair_locked (p : calls_pair) : bool =
-          AtomicPairSet.fold
-            (fun ((((_, psnd') as p'), _) : atomic_pair) : (bool -> bool) ->
-              ( || ) (equal_calls_pair p (if check_first_empty then ("", psnd') else p')) )
-            atomic_last_pairs false
-        in
-        if CallsPairSet.mem p pairs && not (is_pair_locked p) then
-          violations := Violations.add p loc !violations
-      in
-      check p false ;
-      if check_first_empty then check ("", psnd) true
+   (** Checks whether a calls pair is violating atomicity. Returns true if it is a violation. *)
+        method check_violating_atomicity_bool ?(check_first_empty : bool = false)
+            ((_, psnd) as p : calls_pair) ~(atomic_last_pairs : AtomicPairSet.t) : bool =
+          self#init ;
+          let check (p : calls_pair) (check_first_empty : bool) : bool =
+            let is_pair_locked (p : calls_pair) : bool =
+              AtomicPairSet.fold
+                (fun ((((_, psnd') as p'), _) : atomic_pair) acc ->
+                  acc || equal_calls_pair p (if check_first_empty then ("", psnd') else p'))
+                atomic_last_pairs false
+            in
+            CallsPairSet.mem p pairs && not (is_pair_locked p)
+          in
+          check p false || (check_first_empty && check ("", psnd) true)
+
   end
 
 (** An instance of the 'atomic_pairs' class. *)
 let atomic_pairs : atomic_pairs = new atomic_pairs
 
 (* ************************************ Astate ************************************************** *)
+let equal_pairs (p1a, p1b) (p2a, p2b) : bool =
+    let comparison1 = String.equal p1a p2a in
+    let comparison2 = String.equal p1b p2b in
+    let comparison3 = String.equal p1a p2b in
+    let comparison4 = String.equal p1b p2a in
+    (comparison1 && comparison2) || (comparison3 && comparison4)
+
+(* Type to store correct usage count *)
+type correct_usage_count = calls_pair * int
+[@@deriving compare, equal, show]
+
+module CorrectUsageCountOrd = struct
+  type t = correct_usage_count
+
+  let equal (p1, _) (p2, _) = equal_pairs p1 p2
+
+  let compare (p1, _) (p2, _) =
+    if equal_pairs p1 p2 then 0 else Stdlib.compare p1 p2
+
+  let pp fmt (pair, count) =
+    Format.fprintf fmt "%a (correct count: %d)" pp_calls_pair pair count
+end
+
+module CorrectUsageCountSet = MakePPSet(CorrectUsageCountOrd)
+type correct_usage_count_set = CorrectUsageCountSet.t
+
+let add_to_correct_usage_set (pair : calls_pair) (set : correct_usage_count_set) : correct_usage_count_set =
+  let found = CorrectUsageCountSet.fold
+    (fun (existing_pair, count) acc ->
+      if equal_pairs existing_pair pair then Some (existing_pair, count) else acc
+    )
+    set None
+  in
+  match found with
+  | Some (existing_pair, count) ->
+      let set = CorrectUsageCountSet.remove (existing_pair, count) set in
+      CorrectUsageCountSet.add (existing_pair, count + 1) set
+  | None ->
+      CorrectUsageCountSet.add (pair, 1) set
+
 
 (** An element of an abstract state. *)
 type t_element =
@@ -188,9 +226,11 @@ type t_element =
   ; last_pair: calls_pair
   ; nested_last_calls: CallSet.t
   ; violations: Violations.t
+  ; correct_usage_counts: CorrectUsageCountSet.t
   ; atomic_last_pairs: AtomicPairSet.t
   ; guards: Guards.t
-  ; calls: CallSet.t }
+  ; calls: CallSet.t
+  }
 [@@deriving compare, equal, show {with_path= false}]
 
 (** A set of types 't_element' is an abstract state. *)
@@ -213,11 +253,11 @@ let initial : t =
     ; last_pair= empty_calls_pair
     ; nested_last_calls= CallSet.empty
     ; violations= Violations.empty
+    ; correct_usage_counts= CorrectUsageCountSet.empty
     ; atomic_last_pairs= AtomicPairSet.empty
     ; guards= Guards.empty
     ; calls= CallSet.empty }
 
-(* ToDO Add a new field to summary and here add the atomic calls which go fine*)
 let apply_call ~(f_name : string) (loc : Location.t) : t -> t =
   let mapper (astate_el : t_element) : t_element =
     let calls_pair_push : calls_pair -> string -> calls_pair = Fn.compose Tuple2.create snd in
@@ -225,15 +265,18 @@ let apply_call ~(f_name : string) (loc : Location.t) : t -> t =
       if String.is_empty astate_el.first_call then f_name else astate_el.first_call
     and last_pair : calls_pair = calls_pair_push astate_el.last_pair f_name
     and violations : Violations.t ref = ref astate_el.violations
+    and correct_usage_counts : CorrectUsageCountSet.t ref = ref astate_el.correct_usage_counts
     and atomic_last_pairs : AtomicPairSet.t =
-      (* Updates pairs of atomic function calls. *)
       AtomicPairSet.map
         (fun ((p, lock) : atomic_pair) : atomic_pair -> (calls_pair_push p f_name, lock))
         astate_el.atomic_last_pairs
-    and calls : CallSet.t = CallSet.add f_name astate_el.calls in
-    (* Check whether the last pair is violating atomicity. *)
-    atomic_pairs#check_violating_atomicity last_pair ~violations ~atomic_last_pairs loc
-      ~check_first_empty:true ;
+    and calls : CallSet.t = CallSet.add f_name astate_el.calls
+    in
+    (* Check last_pair for violation or correct usage *)
+    if atomic_pairs#check_violating_atomicity_bool last_pair ~atomic_last_pairs ~check_first_empty:true then
+      violations := Violations.add last_pair loc !violations
+    else
+      correct_usage_counts := add_to_correct_usage_set last_pair !correct_usage_counts ;
     let iterator (last_call : string) : unit =
       let p : calls_pair = (last_call, f_name) in
       let atomic_last_pairs : AtomicPairSet.t =
@@ -242,21 +285,22 @@ let apply_call ~(f_name : string) (loc : Location.t) : t -> t =
             ((if String.is_empty pfst then p' else p), lock) )
           atomic_last_pairs
       in
-      (* Check whether each pair beginning with the nested last call and ending with the current
-         function call is violating atomicity. *)
-      atomic_pairs#check_violating_atomicity p ~violations ~atomic_last_pairs loc
+      if atomic_pairs#check_violating_atomicity_bool p ~atomic_last_pairs ~check_first_empty:false then
+        violations := Violations.add p loc !violations
+      else
+        correct_usage_counts := add_to_correct_usage_set p !correct_usage_counts
     in
     CallSet.iter iterator astate_el.nested_last_calls ;
     { astate_el with
       first_call
     ; last_pair
-    ; nested_last_calls= CallSet.empty
-    ; violations= !violations
+    ; nested_last_calls = CallSet.empty
+    ; violations = !violations
+    ; correct_usage_counts = !correct_usage_counts
     ; atomic_last_pairs
     ; calls }
   in
   TSet.map mapper
-
 
 let apply_locks (locks_paths : AccessPath.t list) : t -> t =
   let mapper ({guards; atomic_last_pairs} as astate_el : t_element) : t_element =
@@ -339,19 +383,16 @@ let widen ~(prev : t) ~(next : t) ~(num_iters : int) : t =
 (* ************************************ Summary ************************************************* *)
 
 module Summary = struct
-  type t =
-    {first_calls: CallSet.t; last_calls: CallSet.t; violations: Violations.t; calls: CallSet.t}
+   type t =
+    { first_calls: CallSet.t
+    ; last_calls: CallSet.t
+    ; violations: Violations.t
+    ; correct_usage_counts: CorrectUsageCountSet.t
+    ; calls: CallSet.t }
   [@@deriving compare, equal, show {with_path= false}]
 
   type violation_count = calls_pair * int
   [@@deriving compare, equal, show]
-
-  let equal_pairs (p1a, p1b) (p2a, p2b) : bool =
-    let comparison1 = String.equal p1a p2a in
-    let comparison2 = String.equal p1b p2b in
-    let comparison3 = String.equal p1a p2b in
-    let comparison4 = String.equal p1b p2a in
-    (comparison1 && comparison2) || (comparison3 && comparison4)
 
     let compare_violation_count (v1 : violation_count) (v2 : violation_count) : int =
       let (pair1, count1) = v1 in
@@ -397,20 +438,27 @@ module Summary = struct
       result
 
 
-  let create (astate : astate) : t =
+ let create (astate : astate) : t =
     let first_calls : CallSet.t ref = ref CallSet.empty
     and last_calls : CallSet.t ref = ref CallSet.empty
     and s_violations : Violations.t ref = ref Violations.empty
+    and s_correct_usage_counts : CorrectUsageCountSet.t ref = ref CorrectUsageCountSet.empty
     and s_calls : CallSet.t ref = ref CallSet.empty in
-    let iterator ({first_call; last_pair; violations; calls} : t_element) : unit =
+
+    let iterator ({first_call; last_pair; violations; correct_usage_counts; calls} : t_element) : unit =
       if not (String.is_empty first_call) then first_calls := CallSet.add first_call !first_calls ;
       if not (String.is_empty (snd last_pair)) then
         last_calls := CallSet.add (snd last_pair) !last_calls ;
       s_violations := Violations.union violations !s_violations ;
+      s_correct_usage_counts := CorrectUsageCountSet.union correct_usage_counts !s_correct_usage_counts ;
       s_calls := CallSet.union calls !s_calls
     in
     TSet.iter iterator astate ;
-    {first_calls= !first_calls; last_calls= !last_calls; violations= !s_violations; calls= !s_calls}
+    { first_calls= !first_calls
+    ; last_calls= !last_calls
+    ; violations= !s_violations
+    ; correct_usage_counts= !s_correct_usage_counts
+    ; calls= !s_calls }
 
 
   let is_top_level_fun (pname : Procname.t) : (Procname.t * t) list -> bool =
@@ -444,20 +492,19 @@ module Summary = struct
         summaries
 
 
-  let filter_summary (violationCountSet : violation_count_set) (summary : Procname.t * t) : Procname.t * t =
-      let proc, {first_calls; last_calls; violations; calls} = summary in
-      let new_violations =
-        Violations.fold
-          ~f:(fun (pair, loc, _) acc ->
-            match find_pair pair violationCountSet with
-            | Some count when count >= Config.atomicity_violation_min_limit_to_print ->
-                Violations.add pair loc acc
-            | _ -> acc)
-          violations
-          Violations.empty
-      in
-      (proc, {first_calls; last_calls; violations = new_violations; calls})
-
+ let filter_summary (violationCountSet : violation_count_set) (summary : Procname.t * t) : Procname.t * t =
+  let proc, state = summary in
+  let new_violations =
+    Violations.fold
+      ~f:(fun (pair, loc, _) acc ->
+        match find_pair pair violationCountSet with
+        | Some count when count >= Config.atomicity_violation_min_limit_to_print ->
+            Violations.add pair loc acc
+        | _ -> acc)
+      state.violations
+      Violations.empty
+  in
+  (proc, {state with violations = new_violations})
 
   let filter_summaries (summaries : (Procname.t * t) list) =
       if Config.atomicity_violation_min_limit_to_print <= 1 then
@@ -497,29 +544,38 @@ module Summary = struct
     Violations.fold ~f:fold violations
 end
 
-let apply_summary ({first_calls; last_calls; violations= s_violations} : Summary.t)
+let apply_summary ({first_calls; last_calls; violations = s_violations} : Summary.t)
     (loc : Location.t) : t -> t =
   if CallSet.is_empty first_calls && CallSet.is_empty last_calls && Violations.is_empty s_violations
   then Fn.id
   else
-    let mapper ({last_pair; atomic_last_pairs; violations} as astate_el : t_element) : t_element =
+    let mapper ({last_pair; atomic_last_pairs; violations; correct_usage_counts} as astate_el : t_element) : t_element =
       let violations : Violations.t ref =
         let summary_violations : Violations.t =
           if AtomicPairSet.is_empty atomic_last_pairs then s_violations
           else Violations.make_all_warnings s_violations
         in
         ref (Violations.union violations summary_violations)
+      and correct_usage_counts : CorrectUsageCountSet.t ref = ref correct_usage_counts
       and last_call : string = snd last_pair in
       let iterator (first_call : string) : unit =
         let p : calls_pair = (last_call, first_call) in
-        let atomic_last_pairs : AtomicPairSet.t =
+        let atomic_last_pairs_mapped : AtomicPairSet.t =
           AtomicPairSet.map (Fn.compose (Tuple2.create p) snd) atomic_last_pairs
         in
-        (* Check whether each pair beginning with the last called function and ending witch the
-           first call of a given summary is violating atomicity. *)
-        atomic_pairs#check_violating_atomicity p ~violations ~atomic_last_pairs loc
+        let is_violation =
+          atomic_pairs#check_violating_atomicity_bool p ~atomic_last_pairs:atomic_last_pairs_mapped
+        in
+        if is_violation then
+          violations := Violations.add p loc !violations
+        else
+          correct_usage_counts := add_to_correct_usage_set p !correct_usage_counts
       in
       CallSet.iter iterator first_calls ;
-      {astate_el with nested_last_calls= last_calls; violations= !violations}
+      { astate_el with
+        nested_last_calls = last_calls
+      ; violations = !violations
+      ; correct_usage_counts = !correct_usage_counts }
     in
     TSet.map mapper
+
